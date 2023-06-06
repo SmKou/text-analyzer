341922fd364bd53feb88b22808ae79eb7f06f87b
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-06 11:01:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Text Analyzer
+
+By: Jake Elsberry and Stella Marie
+
+Demonstration app of vanilla js and loops.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Skeleton 2.0.4 by Dave Gamache
+- Vanilla JS ()
+
+## Description
+
+This app uses a form to collect text and returns information about the text. Analysis consists of the number of words and sentences, and optionally, the number of instances of a given word.
---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/**
+ * Functionality:
+ * - count number of words in text passage
+ * - count number of sentences in text passage
+ * - count number of instances of word in text passage
+ * - locate phrase, word or part of word throughout text passage
+ * 
+ * Functionality: Omit offensive words from text passage
+ * Ex.
+ * storage: ['zoinks', 'muppeteer', 'biffaroni', 'loopdaloop']
+ * @param text {String} "There are zoinks dogs everywhere, biffaroni!"
+ * @return text {String} "There are dogs everywhere!"
+ * Source: https://www.learnhowtoprogram.com/introduction-to-programming/arrays-and-looping/practice-extending-text-analyzer-business-logic-with-tdd
+ * 
+ * Writing a test:
+ * Describe: functionName()
+ * Test: description
+ * Code: input, call function, output
+ * Expected output: value|object
+ */
